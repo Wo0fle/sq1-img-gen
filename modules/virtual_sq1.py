71e16bbb293959a198ef9af7e73e33c56041c77d
Woofle <officiallywoofle@gmail.com>
2024-06-12 16:23:20 -0400

    implement Layer turns
    it doesnt work past 12 but thats fine since ill limit it to 6 max... somehow

--- modules/virtual_sq1.py
+++ modules/virtual_sq1.py
@@ -1,4 +1,6 @@
 class Square1:
+    """A virtual Square-1 object."""
+
     def __init__(self) -> None:
         top = Layer("A1B2C3D4")
         bottom = Layer("5E6F7G8H")
@@ -23,24 +25,82 @@ class Square1:
     def flip_equator(self):
         self.equator_flipped = not self.equator_flipped
 
-    def slash(self): # lol slice is taken by Python already
+    def slash(self): # lol "slice" is taken by Python already
         pass
 
 #########################################################
 
 class Layer:
-    current_state = ""
-    solved_state = ""
-    sliceable = True
+    """An arbitrary layer of the Square-1."""
 
-    def __init__(self, solved_state) -> None:
-        self.current_state = solved_state
-        self.solved_state = solved_state
+    def __init__(self, initial_state:str) -> None:
+        self.current_state = initial_state
+        self.sliceable = self.check_sliceability()
     
     def __str__(self) -> str:
         return str(self.current_state)
+    
+    def check_sliceability(self):
+        value = 0
 
-    def turn(self):
-        pass
+        for piece in self.current_state:
+            #print("piece: " + str(piece))
 
-# doesnt seem too bad, the square-1 is a fundamentally simple puzzle... hopefully the way im doing this isn't wildly inefficient lol (it probably is)
+            if piece in "12345678":
+                value += 1
+            elif piece in "ABCDEFGH":
+                value += 2
+
+            #print("value: " + str(value))
+
+            if value == 6:
+                return True
+            elif value > 6:
+                return False
+        
+
+    def turn(self, amount:int):
+        if amount > 0:
+            initial_state = self.current_state[::-1]
+
+            for piece in initial_state:
+                if piece in "12345678":
+                    value = 1
+                elif piece in "ABCDEFGH":
+                    value = 2
+                
+                if amount >= value:
+                    amount -= value
+                    self.current_state = piece + self.current_state[:-1]
+                elif value > amount:
+                    if amount != 0:
+                        print(f"Incomplete turn: layer rotated to an unsliceable position. Reset to initial position.")
+                        self.current_state = initial_state[::-1]
+
+                    break
+        elif amount < 0:
+            amount *= -1
+            initial_state = self.current_state
+
+            for piece in initial_state:
+                if piece in "12345678":
+                    value = 1
+                elif piece in "ABCDEFGH":
+                    value = 2
+                
+                if amount >= value:
+                    amount -= value
+                    self.current_state = self.current_state[1:] + piece
+                elif value > amount:
+                    if amount != 0:
+                        print(f"Incomplete turn: layer rotated to an unsliceable position. Reset to initial position.")
+                        self.current_state = initial_state
+
+                    break
+
+#########################################################
+
+sq1 = Square1()
+print(sq1.bottom)
+sq1.bottom.turn(2)
+print(sq1.bottom)
