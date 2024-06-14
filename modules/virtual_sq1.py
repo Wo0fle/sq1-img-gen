34b4e5db53b1d60a8854135aa0b509fc723e6276
Woofle <officiallywoofle@gmail.com>
2024-06-14 14:22:39 -0400

    added "Case" input type!
    and its error handling!
    
    final input type is "state" and then... on to image gen :scream:

--- modules/virtual_sq1.py
+++ modules/virtual_sq1.py
@@ -53,7 +53,11 @@ class Square1:
         self.flip_equator()
     
     def apply_alg(self, alg:str, for_case:bool=False) -> None:
-        """Applies an input algorithm to the Square1, starting from a solved state."""
+        """
+        If `for_case = False` (default): Applies an input algorithm `alg` to the Square1, starting from a solved state.
+
+        If `for_case = True`: Changes the Square1's state so that the input algorithm `alg` solves it (by inverting the input algorithm and then applying it).
+        """
 
         self.__init__()
 
@@ -65,11 +69,14 @@ class Square1:
 
         simplfied_alg = [slash.split(',') for slash in (''.join(simplfied_alg)).split('/')]
 
+        if for_case:
+            simplfied_alg = self.invert_alg(simplfied_alg)
+
         legal = True
 
-        for turns in simplfied_alg:
+        for i in range(len(simplfied_alg)):
             try:
-                turn_amount = int(turns[0])
+                turn_amount = int(simplfied_alg[i][0])
 
                 while abs(turn_amount) > 6:
                     if turn_amount > 0:
@@ -82,14 +89,14 @@ class Square1:
                     legal = False
                     break
             except:
-                if turns[0] != '':
+                if simplfied_alg[i][0] != '':
                     print('SYNTAX ERROR involving "-" detected!')
                     self.__init__()
                     legal = False
                     break
             
             try:
-                turn_amount = int(turns[1])
+                turn_amount = int(simplfied_alg[i][1])
 
                 while abs(turn_amount) > 6:
                     if turn_amount > 0:
@@ -102,7 +109,7 @@ class Square1:
                     legal = False
                     break
             except ValueError:
-                if turns[1] != '':
+                if simplfied_alg[i][1] != '':
                     print('SYNTAX ERROR involving "-" detected!')
                     self.__init__()
                     legal = False
@@ -110,7 +117,7 @@ class Square1:
             except:
                 pass # assume 0 for bottom turn if there's only one input
             
-            if len(turns) > 2:
+            if len(simplfied_alg[i]) > 2:
                 print('SYNTAX ERROR involving "," detected!')
                 self.__init__()
                 legal = False
@@ -122,27 +129,44 @@ class Square1:
             self.slash()
         else:
             if for_case:
-                self.error_detected(turns, simplfied_alg, 0)
+                self.error_detected(0, simplfied_alg[i], i, simplfied_alg)
             else:
-                self.error_detected(turns, simplfied_alg, 1)
+                self.error_detected(1, simplfied_alg[i], i, simplfied_alg)
     
-    def apply_case(self, case:str) -> None:
-        """Changes the Square1's state so that the input algorithm solves it (by reversing the input algorithm and using the "apply_alg" method)."""
+    def invert_alg(self, simplified_alg:list) -> list:
+        """Inverts the input simplified algorithm `simplified_alg`."""
+        
+        simplified_alg = simplified_alg[::-1]
 
-        ...
+        for i in range(len(simplified_alg)):
+            for j in range(len(simplified_alg[i])):
+                if "-" in simplified_alg[i][j]:
+                    simplified_alg[i][j] = simplified_alg[i][j].replace('-', '', 1)
+                else:
+                    if simplified_alg[i][j] != '0' and simplified_alg[i][j] != '':
+                        simplified_alg[i][j] = "-" + simplified_alg[i][j]
+
+        return simplified_alg
 
     def apply_state(self, state:str) -> None:
-        """Changes the Square1's state to match the input state."""
+        """Changes the Square1's state to match the input state `state`."""
 
         # should i even do this?? or should i just take the input state from the site and directly send it to img_genner? im thinking leave it here for error handling
 
         ...
 
-    def error_detected(self, error_turns:list, error_simplfied_alg:list, input_type:int) -> None:
+    def error_detected(self, input_type:int, error_turns:list=[], error_turns_index:int=0, error_simplfied_alg:list=[]) -> None:
+        """
+        Prints an error message depending on the input type `input_type` (where 0 is "Case", 1 is "Algorithm", and 2 is "State").
+        
+        All other parameters (`error_turns`, `error_turns_index`, `error_simplfied_alg`) are the specifics of where the error occurred, and depend on the `input_type`.
+        """
         if input_type == 0: # case
-            ...
+            self.invert_alg(error_simplfied_alg)
+
+            print(f'Error at "{','.join(error_turns)}" (move #{len(error_simplfied_alg) - error_turns_index}).\nSquare-1 reset.')
         elif input_type == 1: # alg
-            print(f'Error at "{','.join(error_turns)}" (move #{error_simplfied_alg.index(error_turns) + 1}).\nSquare-1 reset.')
+            print(f'Error at "{','.join(error_turns)}" (move #{error_turns_index + 1}).\nSquare-1 reset.')
         elif input_type == 2: # state
             ...
 
@@ -152,7 +176,7 @@ class Layer:
     """An arbitrary layer of the Square1."""
 
     def __init__(self, initial_state:str) -> None:
-        """Initializes the Layer. Initial state is input."""
+        """Initializes the Layer. Initial state `initial_state` is input."""
 
         self.current_state = initial_state
     
@@ -161,7 +185,7 @@ class Layer:
 
         return str(self.current_state)
     
-    def is_sliceable(self) -> bool:
+    def is_sliceable(self):
         """Determines whether or not the Layer can slice/slash at the current position."""
 
         value = 0
@@ -175,10 +199,10 @@ class Layer:
             if value == 6:
                 return True
             elif value > 6:
-                return False
+                return False            
 
-    def turn(self, amount:int) -> bool:
-        """Rotates the Layer by an input amount."""
+    def turn(self, amount:int):
+        """Rotates the Layer by an input amount `amount`."""
 
         if amount > 0:
             initial_state = self.current_state[::-1]
@@ -227,5 +251,5 @@ class Layer:
 sq1 = Square1()
 
 print(f"\nBefore: {sq1}\n")
-sq1.apply_alg("(-2,0)/ (-3,6)/ (0,-3)/ (0,-3)/ (5,-1)/ (-3,0)/ (0,-5)/ (0,-3)/ (0,-1)/ (0,-4)/ (4,0)/ (4,0)/ (0,-4)/ (6,0)")
+sq1.apply_alg("(1,0) / (-3,3) / (3,-3) / (-1,0) / (0,3) / (0,-3) / (0,3) / (0,-3) /", True)
 print(f"\nAfter: {sq1}\n")
